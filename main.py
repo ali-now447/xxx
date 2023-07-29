49909f6ffb169c02fb3c22867015ba00df91ad90
Raqawi <134682323+Abnsyria03@users.noreply.github.com>
2023-07-30 00:13:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,4 @@ if not db.get('sessions'):
   db.set('sessions', [])
 if not db.get('force'):
   db.set('force', ['trprogram'])
-x = Client(name='loclhosst', api_id=29848011, api_hash='ab9bd73716cfe9939ea5ff0bd9bad498', bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
-
-x.run()
+x = Client(name='loclhosst', api_id=29848011, api_hash='ab9bd73716cfe9939ea5ff0bd9bad498', bot_token=tok, 
