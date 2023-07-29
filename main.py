acd23ea3d369d51e2b6f00986f52bd4987b79d16
Raqawi <134682323+Abnsyria03@users.noreply.github.com>
2023-07-30 00:00:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import pyrogram , pyromod
+
+from pyromod import listen
+
+from pyrogram import Client, filters, enums
+from kvsqlite.sync import Client as dt
+p = dict(root='plugins')
+tok = '6364320228:AAEDFtk7nN9cH_1KGJBQ-4DBvHxX8SsGt6M' ## توكنك 
+id = 5108562302
+db = dt("data.sqlite", 'fuck')
+if not db.get("checker"):
+  db.set('checker', None)
+if not db.get("admin_list"):
+  db.set('admin_list', [id], 5108562302)
+if not db.get('ban_list'):
+  db.set('ban_list', [])
+if not db.get('sessions'):
+  db.set('sessions', [])
+if not db.get('force'):
+  db.set('force', ['trprogram'])
+x = Client(name='loclhosst', api_id=29848011, api_hash='ab9bd73716cfe9939ea5ff0bd9bad498', bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
+
+x.run()
